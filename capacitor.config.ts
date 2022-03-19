e3103fe4c479c29cd2f28485e579b39b61673486
Suraj Bastola <bastolasuraj@hotmail.com>
2022-03-19 15:12:42 -0400

    converted ts to js

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Learning to Create Vue App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
